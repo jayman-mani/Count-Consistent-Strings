0481c6e434ec4e8b4094215bda7f165726613575
Jayakrishnan Mani <jayakrishnan_mani@homedepot.com>
2022-06-21 17:55:55 -0700

    Initial commit

---
+++ CountConsistentStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountConsistentStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
